f08a7d48b1a37552c94d6b21ab422c197b4c1dcc
Jawad Iqbal <jawad.iqbal92@gmail.com>
2019-05-16 01:56:05 +0600

    added support for ftp

--- src/server/api/download.js
+++ src/server/api/download.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const axios = require('axios');
 const path = require('path');
 const checkDiskSpace = require('check-disk-space');
+const Ftp = require('ftp');
 
 const {
   DOWNLOAD_PATH
@@ -12,6 +13,7 @@ function download(req, res) {
   debug('requesting to /api/v1/download | POST | params: ', req.body);
 
   const {
+    protocol,
     url
   } = req.body;
 
@@ -26,57 +28,111 @@ function download(req, res) {
 
   const file = fs.createWriteStream(destination);
 
-  axios({
-    method: 'get',
-    url,
-    responseType: 'stream'
-  }).then(response => {
-    const fileSizeInHost = response.headers['content-length'];
-    debug('File size in host: ', fileSizeInHost);
+  if (protocol === 'ftp') {
+    const {
+      config
+    } = req.body;
+    // let securityConfig = {}
 
-    checkDiskSpace(DOWNLOAD_PATH).then((diskSpace) => {
-      debug('Disk size:          ', diskSpace.size);
-      debug('Free space in disk: ', diskSpace.free);
-      if (diskSpace.free < fileSizeInHost) {
-        debug('Operation failed: not enough space in disk');
-      } else {
-        response.data.pipe(file);
+    if (!('host' in config)) {
+      config.host = args[2];
+    }
+
+    // if (protocol === 'ftps') {
+    //   securityConfig = {
+    //     secure: true,
+    //     secureOptions: {
+    //       rejectUnauthorized: false
+    //     }
+    //   }
+    // }
+
+    let filePath = args[3];
+    for (let i = 4; i < args.length; i += 1) {
+      filePath += `//${args[i]}`;
+    }
+
+    debug('File path: ', filePath)
+
+    const c = new Ftp();
+
+    c.on('ready', () => {
+      c.get(filePath, (err, stream) => {
+        if (err) throw err;
+        stream.once('close', () => {
+          c.end();
+        });
+        stream.pipe(file);
         file.on('finish', () => {
           file.close(() => {
-            const downloadedFileSizeInBytes = fs.statSync(destination).size;
-            debug('Downloaded file size: ', downloadedFileSizeInBytes);
+            // const downloadedFileSizeInBytes = fs.statSync(destination).size;
+            // debug('Downloaded file size: ', downloadedFileSizeInBytes);
 
-            // eslint-disable-next-line eqeqeq
-            if (downloadedFileSizeInBytes != fileSizeInHost) {
-              debug(downloadedFileSizeInBytes, ' vs ', fileSizeInHost);
-              fs.unlink(destination, () => {
-                // return res.status(500).send({
-                //   message: 'Download incomplete! removing file from disk.'
-                // });
-                debug('Operation failed: incomplete download');
-              })
-            };
-
-            // return res.status(200).send({
-            //   message: 'Operation Successful!'
-            // });
+            // // eslint-disable-next-line eqeqeq
+            // if (downloadedFileSizeInBytes != fileSizeInHost) {
+            //   debug(downloadedFileSizeInBytes, ' vs ', fileSizeInHost);
+            //   fs.unlink(destination, () => {
+            //     // return res.status(500).send({
+            //     //   message: 'Download incomplete! removing file from disk.'
+            //     // });
+            //     debug('Operation failed: incomplete download');
+            //   })
+            // };
             debug('Operation successful: file saved to ', destination);
           });
         });
-      }
-    });
-  }).catch(
-    error => {
-      fs.unlink(destination, () => {
-        file.close();
       });
-      debug(error);
-      // return res.status(400).send({
-      //   message: 'Cannot download file from target host!'
-      // });
-      debug('Operation failed: host/network error');
-    }
-  );
+    });
+
+    c.connect({
+      ...config
+      // ...securityConfig
+    });
+  }
+
+  if (protocol === 'http' || protocol === 'https') {
+    axios({
+      method: 'get',
+      url,
+      responseType: 'stream'
+    }).then(response => {
+      const fileSizeInHost = response.headers['content-length'];
+      debug('File size in host: ', fileSizeInHost);
+
+      checkDiskSpace(DOWNLOAD_PATH).then((diskSpace) => {
+        debug('Disk size:          ', diskSpace.size);
+        debug('Free space in disk: ', diskSpace.free);
+        if (diskSpace.free < fileSizeInHost) {
+          debug('Operation failed: not enough space in disk');
+        } else {
+          response.data.pipe(file);
+          file.on('finish', () => {
+            file.close(() => {
+              const downloadedFileSizeInBytes = fs.statSync(destination).size;
+              debug('Downloaded file size: ', downloadedFileSizeInBytes);
+
+              // eslint-disable-next-line eqeqeq
+              if (downloadedFileSizeInBytes != fileSizeInHost) {
+                debug(downloadedFileSizeInBytes, ' vs ', fileSizeInHost);
+                fs.unlink(destination, () => {
+                  debug('Operation failed: incomplete download');
+                })
+              };
+              debug('Operation successful: file saved to ', destination);
+            });
+          });
+        }
+      });
+    }).catch(
+      error => {
+        fs.unlink(destination, () => {
+          file.close();
+        });
+        debug(error);
+        debug('Operation failed: host/network error');
+      }
+    );
+  }
 
   return res.status(200).send({
     message: 'Request accepted successfully!'
